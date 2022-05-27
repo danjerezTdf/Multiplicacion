64b85a9f91aaf8b7b84e4d9436c6948883037acf
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:44:26 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+/*
+Multiplicación
+•Implemente un método llamado “multiplicarArreglo” que recibe como parámetros tres arreglos de Enteros del mismo tamaño
+•Los dos primeros arreglos contienen los números que se quieren multiplicar
+•El tercer arreglo almacena el cálculo de la multiplicación de cada posición de los dos arreglos
+•Utilice este método para multiplicar los siguientes cuatro arreglos de tres elementos
+
+aproveche las ventajas de métodos para resolver el ejercicio
+v1: [1, 2, 3] v2: [4, 5, 6] v3: [2, 1, 2] v4: [1, 2, 1]
+vResultado (v1*v2*v3*v4): [8, 20, 36]
+*/
+
 let vectorUno: number[] = [1, 2, 3];
 let vectorDos: number[] = [4, 5, 6];
 let vectorTres: number[] = [2, 1, 2];
